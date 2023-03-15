1c057cb5a709cb704d1377d106a99227297c9726
ionel71089 <ionel.lescai@flir.com>
2023-03-15 13:46:33 +0200

    Change pod repo

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -34,6 +34,6 @@ kmmbridge {
     mavenPublishArtifacts()
     githubReleaseVersions()
     spm()
-    cocoapods("git@github.com:touchlab/PublicPodspecs.git")
+    cocoapods("git@github.com:ionel71089/PublicPodspecs.git")
     versionPrefix.set("0.8")
 }
