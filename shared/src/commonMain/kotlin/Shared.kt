0704a1b0233a2c41a272d8d0dfceff03267da376
ionel71089 <ionel.lescai@flir.com>
2023-04-04 12:05:12 +0300

    Revert "Add supanatural"
    This reverts commit 9b1e7d70bdc295574a87574d75e0733ff06f56af.

--- shared/src/commonMain/kotlin/Shared.kt
+++ shared/src/commonMain/kotlin/Shared.kt
@@ -1,10 +1,6 @@
-import com.suparnatural.core.fs.FileSystem
-
 class Shared {
     fun getString(): String {
-        val documentsDirectory = FileSystem.contentsDirectory
-        val isFileCreated = FileSystem.touch(documentsDirectory.absolutePath?.byAppending("test.txt")!!)
-        println("Running in Shared $isFileCreated")
-        return "Hello from Sprint Review! $isFileCreated"
+        println("Running in Shared")
+        return "Hello from Sprint Review!"
     }
 }
