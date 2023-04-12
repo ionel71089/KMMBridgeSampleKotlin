e651e26438200fb0933d02c26304ce0d2b72fb60
ionel71089 <ionel.lescai@flir.com>
2023-04-12 11:27:47 +0300

    kmm 1.7.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.7.10" apply false
+    kotlin("multiplatform") version "1.7.20" apply false
     id("com.android.library") version "7.4.2" apply false
 }
 
